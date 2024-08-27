82ac5e465faa84fa3385f55feadba45800b13075
ibrahim <ibrahim.ozcan@finartz.com>
2024-08-27 11:43:00 +0300

    Update Content-Security-Policy and X-Frame-Options meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.google.com https://*.gstatic.com https://*.googletagmanager.com; style-src 'self' 'unsafe-inline' https://*.googleapis.com; img-src 'self' data: https: blob:; connect-src 'self' https: ws:; font-src 'self' https: data:; frame-src 'self' https://*.google.com; object-src 'none';" />
+      <meta httpEquiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'nonce-fcd72090-1202-4a55-937b-b835727ed135' 'unsafe-inline' 'unsafe-eval' https://*.google.com; style-src 'self' 'unsafe-inline' https://*.googleapis.com; img-src 'self' data: https: blob:; connect-src 'self' https: data:; font-src 'self' https://*.googleapis.com; frame-src 'none'; object-src 'none';"/>
       <meta httpEquiv="X-Frame-Options" content="SAMEORIGIN" />
       <body className={inter.className}>{children}</body>
     </html>
